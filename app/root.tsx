fe700ee7ef8fa08d163825457a0d5d3990c9b120
Isaac Vallee <i.vallee@astoundcommerce.com>
2022-11-05 20:24:54 -0700

    Scaffold main routes

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,15 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  Link,
+  NavLink,
+  Form
 } from "@remix-run/react";
+
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
+import { useOptionalUser } from "~/utils";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
@@ -29,6 +34,7 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 export default function App() {
+  const user = useOptionalUser();
   return (
     <html lang="en" className="h-full">
       <head>
@@ -36,7 +42,62 @@ export default function App() {
         <Links />
       </head>
       <body className="h-full">
-        <Outlet />
+        <div className="flex h-full min-h-screen flex-col">
+          <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+            <h1 className="text-3xl font-bold">
+              <Link to=".">Halfsies</Link>
+            </h1>
+            <p>{user ? user.email : "Not signed in!"}</p> 
+            {user ? (
+              <Form action="/logout" method="post">
+                <button
+                  type="submit"
+                  className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                >
+                  Logout
+                </button>
+              </Form>
+            ) : (
+              <Link to="/login">Log in</Link>
+            )}
+          </header>
+
+          {/* TODO: Implement signed out state homepage */}
+          <main className="flex h-full bg-white">
+            <div className="h-full w-80 border-r bg-gray-50">
+              <ul>
+                <li>
+                  <NavLink 
+                    to="dashboard"
+                    className={({ isActive }) => `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`}
+                    >
+                    Dashboard
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="friends"
+                    className={({ isActive }) => `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`}
+                    >
+                      Friends
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink 
+                    to="/groups"
+                    className={({ isActive }) => `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`}
+                  >
+                    Groups
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex-1 p-6">
+              <Outlet />
+            </div>
+          </main>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
