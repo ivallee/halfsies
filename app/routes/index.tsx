71cd08ed1268534564e30e0d509aecd9db4fb6b5
Isaac Vallee <i.vallee@astoundcommerce.com>
2022-11-06 19:52:12 -0800

    test commit for fly

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,17 @@
-import DashboardPage from "./dashboard";
+import type { LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { getUser } from "~/session.server";
+
+export async function loader({ request }: LoaderArgs)  {
+  const user = await getUser(request);
+  if (user) {
+    return redirect("dashboard");
+  }
+  return json({});
+}
+
 export default function Index() {
   return (
-    <DashboardPage/>
+    <p>You aren't even logged in!</p>
   );
 }
